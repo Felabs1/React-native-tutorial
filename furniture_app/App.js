d03e858f7cde615f555482ccfddf9f639c10e695
felabs1 <felixawere01@gmail.com>
2024-01-24 23:15:17 +0300

    design a shopping cart button

--- furniture_app/App.js
+++ furniture_app/App.js
@@ -6,6 +6,7 @@ import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { useCallback } from "react";
 import BottomTabNavigation from "./navigation/BottomTabNavigation";
+import { Cart, Home } from "./screens";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -37,20 +38,12 @@ export default function App() {
           component={BottomTabNavigation}
           options={{ headerShown: false }}
         ></Stack.Screen>
+        <Stack.Screen
+          name="cart"
+          component={Home}
+          options={{ headerShown: false }}
+        ></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  textStyle: {
-    fontSize: 20,
-  },
-});
